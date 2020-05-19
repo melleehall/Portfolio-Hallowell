6f4e0ae5ec6aa3d7818748235ed55a888c511435
Melanie Hallowell <melleehall@gmail.com>
2020-05-19 15:23:22 -0600

    Updated project descriptions

--- src/ProjectSection/ProjectSection.js
+++ src/ProjectSection/ProjectSection.js
@@ -15,7 +15,7 @@ export default class IntroSection extends Component {
                 <h3 className='projects-header'>Recent Projects</h3>
                 <Project 
                     name='Clear Creek Broadband'
-                    description="Clear Creek Broadband (CCB) is a local internet service company located outside of Denver, CO. The purpose of this application is to to provide an easy channel for users to request service, join the mailing list to stay informed, and/or volunteer to help with projects as well as stay updated on network progress and get answers to commonly asked questions." 
+                    description="I created this application for Clear Creek Broadband (CCB), a local internet service company located outside of Denver, CO. The purpose of this application is to provide an easy channel for users to request service, join the mailing list to stay informed, and/or volunteer. The website is also used to keep interested parties updated on network progress and provide answers to commonly asked questions saving CCB Board Members time." 
                     tech = 'Node.js, Express, React, PostgreSQL, JS ES6, HTML5, CSS3'
                     test='Testing: Mocha, Chai, Supertest, Jest'
                     live='https://www.clearcreekbroadband.com/'
@@ -25,7 +25,7 @@ export default class IntroSection extends Component {
                 />
                 <Project 
                     name='Home Holiday'
-                    description="Home holiday is a virtual trip itinerary generator that inspires authentic connection among friends and family. Users can choose from carefully created experiences that will immerse them in the spirit of their destination or create itineraries of their own." 
+                    description="Home holiday is a virtual trip itinerary generator that inspires authentic connection among friends and family. I was inspired by the virtual vacations many people around the globe are creating in their living rooms and backyards while sheltering at home due to COVID-19. Users can choose from carefully created experiences that will immerse them in the spirit of their destination or create itineraries of their own." 
                     tech = 'Node.js, Express, React, PostgreSQL, JS ES6, HTML5, CSS3'
                     test='Testing: Mocha, Chai, Supertest, Jest'
                     live='https://home-holiday.now.sh/'
@@ -35,7 +35,7 @@ export default class IntroSection extends Component {
                     />
                 <Project 
                     name='Economy Pump'
-                    description="Economy Pump was created with the belief that there is power in your dollar. Leveraging the US Census API, Economy Pump shows users the deviation of each US state\'s median household income as compared to the US and then suggests products to purchase directly from a small business located in their chosen area through Etsy\'s API" 
+                    description="Economy Pump was created with the belief that there is power in your dollar and that more people will purchase products Made In The US if they have an easy channel to do so. Leveraging the US Census API, Economy Pump shows users the deviation of each US state's median household income as compared to the US and then suggests products to purchase directly from a small business located in their chosen area and available to purchase from Etsy." 
                     tech = 'JavaScript / jQuery, HTML5, CSS3, Etsy API, US Census API'
                     live='https://melleehall.github.io/EconomyPump/'
                     repo='https://github.com/melleehall/EconomyPump'
