5aaa6b6e06de2c543a0649ac683afbbf2413da1c
Melanie Hallowell <melleehall@gmail.com>
2020-06-04 10:03:15 -0600

    Updated location in Headline

--- src/Headline/Headline.js
+++ src/Headline/Headline.js
@@ -5,7 +5,12 @@ export default class Headline extends Component {
     render() {
         return (
             <section>
-                <h1 className='headline'><span role='img' class="wave" aria-label='jsx-a11y/accessible-emoji'>👋🏽 </span>Hi, I'm Melanie - a software engineer based in the foothills of Boulder, Colorado.</h1>
+                <h1 className='headline'>
+                    <span role='img' class="wave" aria-label='jsx-a11y/accessible-emoji'>
+                        👋🏽 
+                    </span>
+                    Hi, I'm Melanie - a software engineer based in Denver's Capitol Hill.
+                </h1>
             </section>
         )
     }
